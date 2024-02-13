5c5aa1f029c81f417b3d393f957c0a77d6fd4561
cristianmarcelli <cristianma98@gmail.com>
2024-02-13 13:37:47 +0100

    Aggiunto main.dart e categories.dart

---
+++ lib/categories.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class CategoriesScreen extends StatelessWidget {
+  const CategoriesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Pick your category'),
+      ),
+      body: GridView.builder(
+        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20,
+        ),
+        itemBuilder: (BuildContext context, int index) {
+          [
+            const Text('1'),
+            const Text('2'),
+            const Text('3'),
+            const Text('4'),
+            const Text('5'),
+            const Text('6'),
+          ];
+        },
+      ),
+    );
+  }
+}
